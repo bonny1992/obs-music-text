6796567ae2b7ebdce72d1dc95ac382c96b5f1b2b
Alessandro <lh_firepower@hotmail.com>
2018-09-29 21:39:33 +0200

    fixed something

--- spotilib.py
+++ spotilib.py
@@ -8,16 +8,35 @@ Media_Previous = 0xB1
 Media_Pause = 0xB3 ##Play/Pause
 Media_Mute = 0xAD
 
-
+def getHandleByFile():
+	try:
+		with open('handle.txt', 'r') as opened:
+			text = opened.read()
+		if text == '':
+			return False
+		return text
+	except:
+		return False
 
 ###SpotifyInfo###
-def getwindow(Title="SpotifyMainWindow"):
-	window_id = win32gui.FindWindow(Title, None)
-	return window_id
-	
+def getwindow(Title='Spotify', Class=None):
+	general_id = getHandleByFile()
+	if general_id == False:
+		window_id = win32gui.FindWindow(Class, Title)
+		with open('handle.txt', 'w') as opened:
+			opened.write(str(window_id))
+		return window_id
+	return general_id
+	# if general_id == window_id or general_id == False:
+	# 	return general_id
+	# else:
+	# 	general_id = window_id
+	# 	return window_id
+
 def song_info():
 	try:
-		song_info = win32gui.GetWindowText(getwindow())
+		window = int(getwindow())
+		song_info = win32gui.GetWindowText(window)
 	except:
 		pass
 	return song_info
@@ -29,8 +48,8 @@ def artist():
 		artist = artist.strip()
 		return artist
 	except:
-		return "There is noting playing at this moment"
-	
+		return "There is nothing playing at this moment"
+
 def song():
 	try:
 		temp = song_info()
@@ -38,15 +57,15 @@ def song():
 		song = song.strip()
 		return song
 	except:
-		return "There is noting playing at this moment"
-	
+		return "There is nothing playing at this moment"
+
 ###SpotifyBlock###
 def createfolder(folder_path="C:\SpotiBlock"):
 	if not os.path.exists(folder_path):
 		os.makedirs(folder_path)
 	else:
 		pass
-	
+
 def createfile(file_path="C:\SpotiBlock\Block.txt" ):
 	if not os.path.exists(file_path):
 		file = open(file_path, "a")
@@ -58,19 +77,19 @@ def blocklist(file_path="C:\SpotiBlock\Block.txt"):
 		if not line == "":
 			block_list.append(line.strip())
 	return block_list
-	
+
 def add_to_blocklist(file_path="C:\SpotiBlock\Block.txt"):
 	with open(file_path, 'a') as text_file:
 		text_file.write("\n" + song_info())
-		
+
 def reset_blocklist(file_path="C:\SpotiBlock\Block.txt"):
 	with open(file_path, 'w') as text_file:
 		text_file.write("ThisFirstLineWillBeIgnored")
 		pass
-	
-	
 
-	
+
+
+
 ###Media Controls###
 def hwcode(Media):
 	hwcode = win32api.MapVirtualKey(Media, 0)
@@ -78,17 +97,15 @@ def hwcode(Media):
 
 def next():
 	win32api.keybd_event(Media_Next, hwcode(Media_Next))
-	
+
 def previous():
 	win32api.keybd_event(Media_Previous, hwcode(Media_Previous))
-	
+
 def pause():
 	win32api.keybd_event(Media_Pause, hwcode(Media_Pause))
-	
+
 def play():
 	win32api.keybd_event(Media_Pause, hwcode(Media_Pause))
-	
+
 def mute():
 	win32api.keybd_event(Media_Mute, hwcode(Media_Mute))
-	
-	
